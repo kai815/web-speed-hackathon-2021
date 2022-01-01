5d74a58e7f6b7cb1906aa3a91f7fea8e51831df3
kai815 <h.kaizuka815@gmail.com>
2022-01-01 14:14:32 +0900

    cashヘッダーを戻す

--- server/src/app.js
+++ server/src/app.js
@@ -20,6 +20,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw({ limit: '10mb' }));
 
 app.use((_req, res, next) => {
+  res.header({
+    'Cache-Control': 'max-age=0, no-transform',
+    Connection: 'close',
+  });
   return next();
 });
 
