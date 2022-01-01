9007db37335b3cafbeb07c087897d38170816ece
kai815 <h.kaizuka815@gmail.com>
2022-01-01 13:47:33 +0900

    httpヘッダーの変更

--- server/src/app.js
+++ server/src/app.js
@@ -22,10 +22,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw({ limit: '10mb' }));
 
 app.use((_req, res, next) => {
-  res.header({
-    'Cache-Control': 'max-age=0, no-transform',
-    Connection: 'close',
-  });
   return next();
 });
 
--- server/src/routes/static.js
+++ server/src/routes/static.js
@@ -6,27 +6,36 @@ import { CLIENT_DIST_PATH, PUBLIC_PATH, UPLOAD_PATH } from '../paths';
 
 const router = Router();
 
+function setCustomeHeaders (res) {
+  console.log("e")
+  res.setHeader('Content-Encoding','gzip')
+  res.setHeader('cache-control', 'max-age=604800, must-revalidate')
+}
+
 // SPA 対応のため、ファイルが存在しないときに index.html を返す
 router.use(history());
 
 router.use(
   serveStatic(UPLOAD_PATH, {
-    etag: false,
-    lastModified: false,
+    etag: true,
+    lastModified: true,
+    setHeaders:setCustomeHeaders
   }),
 );
 
 router.use(
   serveStatic(PUBLIC_PATH, {
-    etag: false,
-    lastModified: false,
+    etag: true,
+    lastModified: true,
+    setHeaders:setCustomeHeaders
   }),
 );
 
 router.use(
   serveStatic(CLIENT_DIST_PATH, {
-    etag: false,
-    lastModified: false,
+    etag: true,
+    lastModified: true,
+    setHeaders:setCustomeHeaders
   }),
 );
 
